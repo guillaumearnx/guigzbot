95d58ccd9e60f71921837a2ae0c856923130016c
Guillaume ARNOUX <arnouxguillaume54@hotmail.fr>
2021-06-18 01:06:16 +0200

    Add catch to member send

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,20 +2,24 @@
 
 const {MessageEmbed} = require('discord.js');
 const {createLink} = require("../utils/pools.js");
-const {RECAPTCHA} = require("../config.json");
+const {RECAPTCHA, OWNERS, CHANNELS} = require("../config.json");
 const {addRole, welcomeSend} = require("../utils/functions");
 const {getRunning} = require("../webserver.js");
 const empty = [["", " "], [null, undefined]]
 
 module.exports = async (bot, member) => {
     if (getRunning()) {
-        const linkId = createLink(member.id);
-        const embed = new MessageEmbed()
-            .setTitle('reCAPTCHA Verification')
-            .setDescription(`To gain access to this server you must solve a captcha. The link will expire in 15 minutes.\nhttp://${RECAPTCHA["DOMAIN"] === '' ? `localhost:${RECAPTCHA["PORT"]}` : RECAPTCHA["DOMAIN"]}/verify/${linkId}?sitekey=${RECAPTCHA["RECAPTCHA_SITE_KEY"]}`)
-            .setColor('BLUE')
-            .setFooter(`ReCaptcha by ${bot.user.username}`, bot.user.displayAvatarURL())
-        member.send(embed);
+        try{
+            const linkId = createLink(member.id);
+            const embed = new MessageEmbed()
+                .setTitle('reCAPTCHA Verification')
+                .setDescription(`To gain access to this server you must solve a captcha. The link will expire in 15 minutes.\nhttp://${RECAPTCHA["DOMAIN"] === '' ? `localhost:${RECAPTCHA["PORT"]}` : RECAPTCHA["DOMAIN"]}/verify/${linkId}?sitekey=${RECAPTCHA["RECAPTCHA_SITE_KEY"]}`)
+                .setColor('BLUE')
+                .setFooter(`ReCaptcha by ${bot.user.username}`, bot.user.displayAvatarURL())
+            member.send(embed);
+        }catch(){
+            bot.channels.cache.get(CHANNELS["BOT_LOGS"]).send(`<@${OWNERS["ID"]}> | Je n'ai pas réussi à envoyer de message à ${member.user.username}`)
+        }
     } else {
         if (empty[0].indexOf(RECAPTCHA["VERIFIED_ROLE_ID"]) > -1 || empty[1].indexOf(RECAPTCHA["VERIFIED_ROLE_ID"]) > -1) {
             await welcomeSend(bot, member);
