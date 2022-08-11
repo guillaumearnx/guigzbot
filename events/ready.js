74b18ad0a36ca2adc4c673134a4138ae63af4b17
ARNOUX Guillaume <arnouxguillaume54@hotmail.fr>
2022-08-12 01:37:52 +0200

    Discord js V14

--- events/ready.js
+++ events/ready.js
@@ -1,23 +1,21 @@
-const {REST} = require('@discordjs/rest');
-const {BOT_TOKEN, BOT_ID, DISCORD_GUILD_ID} = require("../config.json");
-// noinspection JSClosureCompilerSyntax,JSCheckFunctionSignatures
-const rest = new REST({version: '9'}).setToken(BOT_TOKEN);
-const {Routes} = require('discord-api-types/v9');
-const {reportErr} = require("../utils/functions");
-
+const { CLIENT } = require("../config.json");
+const { deployInteractions, loadInteractions, removeIntegrations } = require("../utils/functions");
 
 module.exports = async (bot) => {
-    console.info(`\nLogged in as ${bot.user.tag}!`.blue);
-    try {
-        bot.user.setActivity(bot.maintenance ? 'la maintenance' : 'garnx.fr', {type: "PLAYING"})
-    } catch (e) {
-        await reportErr(bot, e, "Erreur lors de la gestion des activités")
-    }
-    try {
-        await rest.put(Routes.applicationGuildCommands(BOT_ID, DISCORD_GUILD_ID), {body: Array.from(bot.interactions.values())})
-    } catch (err) {
-        console.error(err);
-        process.exit(-2);
-    }
-    console.log((`A total of ${bot.interactions.size} (/) commands were loaded.`).green);
-}
+	console.info(`\nLogged in as ${bot.user.tag}!`.blue);
+	bot.interactions = await loadInteractions(bot);
+	bot.user.setActivity(`${bot.guilds.cache.get(CLIENT["MAIN_GUILD_ID"]).memberCount} members`, { type: "WATCHING" });
+	try {
+		if (process.env.DEV) {
+			await removeIntegrations();
+			await deployInteractions(!process.env.DEV, bot.interactions);
+		}
+		else {
+			await deployInteractions(!process.env.DEV, bot.interactions);
+		}
+	}
+	catch (e) {
+		console.error(e);
+		process.exit(1);
+	}
+};
--- index.js
+++ index.js
@@ -1,144 +1,83 @@
-//Dépendences
-const {Client, Collection} = require('discord.js');
-const {SlashCommandBuilder} = require('@discordjs/builders');
-const fs = require('fs');
-const recursiveRead = require('recursive-readdir');
-const {runWebServer} = require("./webserver.js");
-const {BOT_TOKEN, BOT_PREFIX, DISCORD_GUILD_ID} = require('./config.json');
-require('colors');
-const {reportErr} = require("./utils/functions");
+// Dependencies
+const { Client, Collection } = require("discord.js");
+const { run } = require("npm-check-updates");
+const fs = require("fs");
+const path = require("path");
+require("colors");
 
-//Verification config
-(() => {
-    const empty = [["", " "], [null, undefined]]
-    if (empty[0].indexOf(BOT_TOKEN) > -1 || empty[1].indexOf(BOT_TOKEN) > -1) {
-        console.log("Please set a bot token !");
-        process.exit(0);
-    }
-    if (empty[0].indexOf(BOT_PREFIX) > -1 || empty[1] === BOT_PREFIX) {
-        console.log("Please set a prefix !");
-        process.exit(0);
-    }
-    if (empty[0].indexOf(DISCORD_GUILD_ID) > -1 || empty[1] === DISCORD_GUILD_ID) {
-        console.log("Please set your main guild id !");
-        process.exit(0);
-    }
-})()
+// Config check
+if (!fs.existsSync("./config.json")) {
+	console.error("Please create a config.json".red);
+	process.exit(-1);
+}
+const empty = ["", " ", null, undefined];
+const { CLIENT } = require("./config.json");
+if (empty.indexOf(CLIENT["TOKEN"]) > -1) {
+	console.error("Please set a bot token !".red);
+	process.exit(0);
+}
+if (empty.indexOf(CLIENT["MAIN_GUILD_ID"]) > -1) {
+	console.error("Please set your main guild id !".red);
+	process.exit(0);
+}
 
-//Variables d'environnement
-let nbCommandes = 0;
-let nbEvents = 0;
-
-//Variable Client
+// Client
 const bot = new Client({
-    intents: 32767
+	intents: 32767,
 });
 
-bot.commands = new Collection();
 bot.aliases = new Collection();
 bot.interactions = new Collection();
-bot.maintenance = fs.existsSync('./maintenance.lock')
 
-console.log(("Lancement du bot ...").brightRed);
+console.log(("Starting bot...").red);
 
-//Évenements et commandes
-fs.readdir('./events/', (err, files) => {
-    if (err) return console.error(err)
-    console.log(`\nÉvènements : ` + `(${files.length})`.yellow);
-    files.forEach(file => {
-        if (!file.endsWith('.js') || file.startsWith('-')) return;
-        nbEvents++
-        const event = require(`./events/${file}`);
-        let eventName = file.split('.')[0];
-        bot.on(eventName, event.bind(null, bot))
-        console.log(`\tChargement de l'évènement : ` + `${eventName}`.blue);
-    })
-    if (nbEvents === 0) console.log("Aucun event actif".yellow)
-});
-
-// noinspection JSIgnoredPromiseFromCall
+// Packages
 (async () => {
-    await recursiveRead('./commands/', (err, files) => {
-        if (err) return console.error(err);
-        console.log(`\nCommandes : ` + `(${files.length})`.yellow);
-        files.forEach((file) => {
-            if (!file.endsWith('.js') || file.startsWith('-')) return
-            let props = require(`./${file}`);
-            let commandName = props.config.name.toLowerCase();
-            bot.commands.set(commandName, props);
-            props.config.aliases.forEach(alias => {
-                bot.aliases.set(alias.toLowerCase(), commandName);
-            });
-            let aliases = props.config.aliases.map(e => e.toString()).join(", ");
-            console.log(`\tChargement de la commande : ` + `${commandName}`.brightRed);
-            console.log(`\t\tRaccourcis : ` + `${aliases}`.cyan);
-            nbCommandes++;
-
-        })
-        if (nbCommandes === 0) console.log("Aucune commande active".yellow)
-    });
+	const r = await run({
+		packageFile: "package.json",
+	});
+	if (Object.keys(r).length > 0) {
+		console.log("Some updates are availables".yellow);
+		for (const [key, value] of Object.entries(r)) {
+			console.log(`${key} -> ${value}`.magenta);
+		}
+	}
+	else {console.log("All packages are up-to-date".magenta);}
 })();
 
-(async () => {
-    await recursiveRead('./interactions/', (err, files) => {
-        if (err) return console.error(err);
-        console.log(`\nInteractions : ` + `(${files.length})`.yellow);
-        files.forEach((file) => {
-            if (!file.endsWith('.js') || file.startsWith('-')) return
-            const interaction = require(`./${file}`);
-            const name = interaction.config.name;
-            const commandBuilder = new SlashCommandBuilder().setName(name).setDescription(interaction.config.description)
-            console.log(`\tChargement de l'interaction : ` + `/${name}`.brightRed);
-            interaction.options.map(op => {
-                const {type} = op;
-                switch (type) {
-                    case 1 :
-                        commandBuilder.addUserOption(option => option.setName(op.name.toLowerCase()).setDescription(op.description));
-                        break;
-                    case 2:
-                        commandBuilder.addRoleOption(option => option.setName(op.name.toLowerCase()).setDescription(op.description));
-                        break;
-                    case 3:
-                        commandBuilder.addChannelOption(option => option.setName(op.name.toLowerCase()).setDescription(op.description));
-                        break;
-                    case 4:
-                        commandBuilder.addStringOption(option => option.setName(op.name.toLowerCase()).setDescription(op.description).setRequired(op.required));
-                        break;
-                    case 5:
-                        commandBuilder.addIntegerOption(option => option.setName(op.name.toLowerCase()).setDescription(op.description));
-                        break;
-                    case 6:
-                        commandBuilder.addBooleanOption(option => option.setName(op.name.toLowerCase()).setDescription(op.description));
-                        break;
-                    case 7:
-                        commandBuilder.addMentionableOption(option => option.setName(op.name.toLowerCase()).setDescription(op.description))
-                        break;
-                }
-            })
-            bot.interactions.set(name, commandBuilder);
-        })
-    });
-})();
-
-//Script
-bot.login(BOT_TOKEN).catch(() => {
-    console.log("Can't connect ...");
-    process.exit(0);
+// Events
+let nbEvents = 0;
+fs.readdir("./events/", (err, files) => {
+	if (err) return console.error(err);
+	console.log(`\n(${files.length}) events`.yellow);
+	files.forEach(file => {
+		if (!file.endsWith(".js") || file.substring(file.indexOf(path.sep) + 1).startsWith("-")) return;
+		nbEvents++;
+		const event = require(`./events/${file}`);
+		const eventName = file.split(".")[0];
+		bot.on(eventName, event.bind(null, bot));
+		console.log("\tLoading event : " + `${eventName}`.blue);
+	});
+	if (nbEvents === 0) console.log("No active events".yellow);
 });
-runWebServer(bot);
 
-//Debug
+// Login to API
+bot.login(CLIENT["TOKEN"]).catch(() => {
+	console.log("Can't connect ...");
+	process.exit(0);
+});
+
+// Debug
 bot.on("warn", (e) => console.warn(e));
-bot.on("error", async (e) => await reportErr(bot, e, 'Oh oh ... Un petit soucis est survenu.'));
-process.on('unhandledRejection', async (error) => {
-    if (!bot.maintenance) await reportErr(bot, error, "Oh oh ... Un petit soucis est survenu")
-}).on('uncaughtException', async (error) => {
-    if (!bot.maintenance) await reportErr(bot, error, "Oh oh ... Un gros soucis est survenu")
-    process.exit(1);
-})
-//bot.on("debug", (e) => console.info(e));
+process.on("unhandledRejection", async (error) => {
+	console.error(error);
+	process.exit(-1);
+}).on("uncaughtException", async (error) => {
+	console.error(error);
+	process.exit(-1);
+});
 
-//Export
+// Export
 module.exports = {
-    bot
-}
+	bot,
+};
